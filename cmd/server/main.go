95d1e207ec67ec8791d6523ee2b2aaa2ef08d244
alex <alex@mathun.io>
2024-03-08 19:04:19 +0300

    iter1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,57 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+type MetricType string
+type MetricName string
+type GaugeValue float64
+type CounterValue int64
+
+const (
+	Gauge   MetricType = "gauge"
+	Counter MetricType = "counter"
+)
+
+func (mt MetricType) IsValid() bool {
+	if mt == Gauge || mt == Counter {
+		return true
+	}
+	return false
+
+}
+
+type MemStorage struct {
+	db []string
+}
+
+type Storage interface {
+	Add(metric_name string) bool
+}
+
+func metricPage(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var mt MetricType = MetricType(r.PathValue("metric_type"))
+	// var mn MetricName = MetricName(r.PathValue("metric_name"))
+
+	if !mt.IsValid() {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(200)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/update/{metric_type}/{metric_name}/{metric_value}", metricPage)
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/screamsoul/go-metrics-tpl
+
+go 1.22.0
